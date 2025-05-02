4191909484baa209ea66063642e44d3699a75404
Lenovo <islomtoxirov08@gmail.com>
2025-05-02 15:20:22 +0500

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterAuthDto, LoginAuthDto, SendOtpDto, VerifyOtpDto } from './dto/create-auth.dto';
+import {
+  RegisterAuthDto,
+  LoginAuthDto,
+  SendOtpDto,
+  VerifyOtpDto,
+} from './dto/create-auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TokenGuard } from 'src/guards/token.guard';
+import { UserRole } from '@prisma/client';
+import { RoleD } from 'src/decorator/role-decorators';
+import { RoleGuard } from 'src/guards/role.guard';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService, 
-    private prisma: PrismaService
+  constructor(
+    private readonly authService: AuthService,
+    private prisma: PrismaService,
   ) {}
-
 
   @Post('register')
   async register(@Body() data: RegisterAuthDto) {
-  
     return this.authService.register(data);
   }
 
@@ -26,19 +44,22 @@ export class AuthController {
     return this.authService.sendOtp(data);
   }
 
+  @RoleD(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
+  @UseGuards(TokenGuard)
   @Post('promote-to-admin')
-  promoteToAdmin(@Body() data: {userId: string}) {
+  promoteToAdmin(@Body() data: { userId: string }) {
     return this.authService.promoteAdmin(data);
   }
-  
+
   @Post('verify-otp')
   verifyOtp(@Body() data: VerifyOtpDto) {
     return this.authService.verifyOtp(data);
   }
 
   @UseGuards(TokenGuard)
-  @Post("me")
-  me( @Request() req) {
-    return this.authService.me( req.user)
+  @Post('me')
+  me(@Request() req) {
+    return this.authService.me(req.user);
   }
 }
